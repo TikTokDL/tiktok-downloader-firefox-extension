f38aaebd9c5178fe613a37f5213339a925d88a60
Hugo Joncour <hjoncour@H-Mac-Mini.local>
2025-03-25 22:19:14 -0400

    dev/build-for-multiple-env

---
+++ env.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+      endpoint: "http://localhost:3000/"
+    },
+    test: {
+      endpoint: "https://test.tiktokze.com/"
+    },
+    production: {
+      endpoint: "https://tiktokze.com/"
+    }
+};
